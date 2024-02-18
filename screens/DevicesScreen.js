8740c66d84a8f6f9c21268d3d17f1856b95c0610
Nikoleta Rasheva <72706965+niki0426@users.noreply.github.com>
2024-02-18 16:39:18 +0200

    Screens

---
+++ screens/DevicesScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const DevicesScreen = () => (
+  <View>
+    <Text>Devices</Text>
+  </View>
+);
+
+export default DevicesScreen;
