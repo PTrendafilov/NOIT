bc0f9d6ed00b6e1f6eac93d80798f2d462f42b6d
Nikoleta Rasheva <72706965+niki0426@users.noreply.github.com>
2024-02-18 16:40:12 +0200

    Screens

---
+++ screens/EditProfileScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const EditProfileScreen = () => (
+  <View>
+    <Text>Edit Profile Page</Text>
+  </View>
+);
+
+export default EditProfileScreen;
