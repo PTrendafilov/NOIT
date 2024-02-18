3940763c5c242af8dabce789349a469e1e00f4da
Nikoleta Rasheva <72706965+niki0426@users.noreply.github.com>
2024-02-18 16:36:52 +0200

    Screens

---
+++ screens/SafetyAreaScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const SafetyAreaScreen = () => (
+  <View>
+    <Text>Safety area</Text>
+  </View>
+);
+
+export default SafetyAreaScreen;
