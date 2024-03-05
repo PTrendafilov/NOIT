7dd05e2234f168382e72548c8ad19ea721dd09ad
Plamen Trendafilov <80783727+PTrendafilov@users.noreply.github.com>
2024-03-06 00:18:01 +0200

    Transferred from the previous research.js
    Removed unused functionalities and renamed used functions

---
+++ GPSProtectionDjango/static/js/device.js
@@ -0,0 +1,8 @@
+function showDeviceForm(){
+    document.getElementById("device-form-container").style.display = "block";
+}
+
+function hideDeviceForm(){
+    document.getElementById("device-form-container").style.display = "none";
+}
+
