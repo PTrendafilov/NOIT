0e69c13f8c4cceb6d0719079c5b727139bf8f0f7
Plamen Trendafilov <80783727+PTrendafilov@users.noreply.github.com>
2024-03-05 23:43:31 +0200

    Changing models.py
    Deleting
        - Parent class
        - Child class
    Adding
        - Notification class
        - Foreign key for the devices' owner

--- GPSProtectionDjango/StaffApp/models.py
+++ GPSProtectionDjango/StaffApp/models.py
@@ -1,17 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
+class Notification(models.Model):
+    text = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,  # If user is deleted, the notifications will be too.
+        related_name='notifications'  # Gives access to a user's notifications with user.notifications.all()
+    )
 
-class Parent(User):
-    children = models.ForeignKey('Child', on_delete=models.CASCADE, related_name='parents', null=True, blank=True)
-    def __str__(self):
-        return f"{self.username} (Parent)"
-
-class Child(User):
-    devices = models.ForeignKey('Device', on_delete=models.CASCADE, related_name='child', null=True, blank=True)
-    def __str__(self):
-        return f"{self.username} (Child)"
-    
 class Device(models.Model):
     class DeviceCategory(models.TextChoices):
         PASSIVE = 'passive', _('Passive')
@@ -23,3 +20,9 @@ class Device(models.Model):
         default=DeviceCategory.PASSIVE,
     )
     name = models.CharField(max_length=255)
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,  # If user is deleted, the devices will be too.
+        related_name='devices'  # Gives access to a user's devices with user.devices.all()
+    )
